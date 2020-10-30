02b557924b18a6441fa35e06ba47ea7996ea0c58
LeeJaeHoon <dlwogns3413@naver.com>
2020-10-30 14:35:18 +0900

    2.8 Router

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => {
-    console.log(`Listening on: http://lcoalhost:${PORT}`);
-} 
 
 const handleHome = (req, res) => {
     res.send("Home");
@@ -30,4 +25,6 @@ app.get("/",handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT,handleListening);
+app.use("/user", userRouter);
+
+export default app;
