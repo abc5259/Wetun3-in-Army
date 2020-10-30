4f5f1d4e54a39b1351bd3111c98b934285de8ac2
LeeJaeHoon <dlwogns3413@naver.com>
2020-10-30 14:14:11 +0900

    middleware2

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Listening on: http://lcoalhost:${PORT}`);
+} 
+
+const handleHome = (req, res) => {
+    res.send("Home");
+}
+
+const handleProfile = (req, res) => {
+    res.send("profile");
+}
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+
+app.get("/",handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT,handleListening);
